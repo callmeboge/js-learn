b51f3e5f407f889bc2a2c579f22cf2254f84504a
bogebaga@gmail.com <bogebaga@gmail.com>
2022-06-25 18:27:37 +0800

    add string literal

--- String.js
+++ String.js
@@ -1,5 +1,24 @@
-let name = "John Doe"
+// String Literal
+// with "", '' and ``
 
-var nameIndex3 = name[3]
+var greetingDoubleQoute = "Hello, my name is John Doe";
+var greetingSingleQoute= 'Hello, my name is John Cena';
+var greetingTick = `Hello, my name is John Travolta`;
 
-console.log(nameIndex3)
+console.log(greetingDoubleQoute);
+console.log(greetingSingleQoute);
+console.log(greetingTick);
+
+// with new line character behavior
+var greetingDoubleQoute = "Hello, my name \n is John Doe";
+var greetingSingleQoute = 'Hello, my name is John Cena';
+var greetingTick = `Hello, \n my name is 
+John  Travolta`;
+
+console.log(greetingDoubleQoute);
+console.log(greetingSingleQoute);
+console.log(greetingTick);
+
+//output:
+// karakter \n akan di identifikasi sebagai baris baru dalam pernyataan string. ketiga format literal string mendukung karakter tersebut. 
+// Khusus string literal dengan tick terdapat fitur-fitur tambahan, seperti dukungan text interpolasi dan men format string keluaran sama dengan format penulisan scriptnya.
